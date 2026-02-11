c9d3c325e415304069e58e82b43c0469ade4ba73
sandeep <somsettysandeep@gmail.com>
2026-02-11 10:49:24 +0530

    my first push

--- myworkspace/Javapractice/src/Day1/Firstprog.java
+++ myworkspace/Javapractice/src/Day1/Firstprog.java
@@ -6,6 +6,8 @@ public class Firstprog {
 	{
 		System.out.println("welcome to java ");
 		System.out.println("hello world");
+		System.out.print("my name is s.sandeep kumar");
+		System.out.println("Welcome to core java");
 		
 	}
 
